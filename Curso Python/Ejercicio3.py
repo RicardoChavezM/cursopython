2f2e31c4ff4c0f9d0981f109e6065c0abb712bf1
RicardoChavezM <104601113+RicardoChavezM@users.noreply.github.com>
2022-05-20 14:21:57 -0500

    Add files via upload

---
+++ Curso Python/Ejercicio3.py
@@ -0,0 +1,15 @@
+
+
+# Entradas de datos
+Año_Naci = float(input('Escribe tu año de nacimiento :'))
+Año_Actual = float(input('Escribe el año de actual :'))
+
+# Proceso de datos
+Edad= Año_Actual-Año_Naci
+
+
+
+# Salidas de datos
+print (f'Tu edad es = { Edad }')
+
+
---
+++ Curso Python/Ejercicio4.py
@@ -0,0 +1,14 @@
+# Entradas de datos
+Grados= float(input('Escribe los grados que se desean convertir :'))
+
+# Proceso de datos
+Grados_Celcius= Grados
+Grados_Fahrenheit= (Grados*1.8)+32
+Grados_Kelvin= Grados + 273.15
+
+
+# Salidas de datos
+print (f'Los grados Celcius son = { Grados_Celcius }')
+print (f'Los grados Fahrenheit son = { Grados_Fahrenheit }')
+print (f'Los grados Kelvin son = { Grados_Kelvin }')
+
