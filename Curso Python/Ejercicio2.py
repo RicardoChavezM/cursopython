f4a03d3ad8406638d839342707b55462770293a2
RicardoChavezM <104601113+RicardoChavezM@users.noreply.github.com>
2022-05-19 20:02:38 -0500

    Add files via upload

---
+++ Curso Python/Ejercicio2.py
@@ -0,0 +1,16 @@
+import math
+
+# Entradas de datos
+Radio = float(input('Escribe el Radio :'))
+
+
+
+# Proceso de datos
+Perimetro= math.pi * 2 * Radio
+Area = math.pi* (Radio ** 2)
+
+
+# Salidas de datos
+print (f'El perimetro del circulo es = { Perimetro }')
+
+print (f'El area del circulo es = { Area }')
